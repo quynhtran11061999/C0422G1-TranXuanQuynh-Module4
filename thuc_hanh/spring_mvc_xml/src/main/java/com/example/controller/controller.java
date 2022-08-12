ef0c85e4d34f7409317d9e8ff69c6b57d8018063
quynhtran11061999 <quynhtran11061999@gmail.com>
2022-08-12 15:29:12 +0700

    update

---
+++ thuc_hanh/spring_mvc_xml/src/main/java/com/example/controller/controller.java
@@ -0,0 +1,11 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class controller {@GetMapping("/")
+    public String spring(){
+        return "/spring";
+    }
+}
