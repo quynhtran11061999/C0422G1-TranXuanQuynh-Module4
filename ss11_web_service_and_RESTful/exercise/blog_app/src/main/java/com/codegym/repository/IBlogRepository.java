f6cc0b8c1234d08f14c158ba93079dbb007d19c8
quynhtran11061999 <quynhtran11061999@gmail.com>
2022-08-31 14:18:37 +0700

    update

---
+++ ss11_web_service_and_RESTful/exercise/blog_app/src/main/java/com/codegym/repository/IBlogRepository.java
@@ -0,0 +1,13 @@
+package com.codegym.repository;
+
+import com.codegym.model.Blog;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface IBlogRepository extends JpaRepository<Blog, Integer> {
+    Page<Blog> findAllByTitleContaining(String title,Pageable pageable);
+    List<Blog> findByCategoryId(Integer id);
+}
---
+++ ss11_web_service_and_RESTful/exercise/blog_app/src/main/java/com/codegym/service/ICategoryService.java
@@ -0,0 +1,12 @@
+package com.codegym.service;
+
+import com.codegym.model.Blog;
+import com.codegym.model.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface ICategoryService {
+    Page<Category> findAll(Pageable pageable);
+}
