04d96a1ea0b096e25589f866212003ff3906ece1
quynhtran11061999 <quynhtran11061999@gmail.com>
2022-09-07 11:29:28 +0700

    update

--- ss14_spring_security/exercise/blog_app/src/main/java/com/codegym/config/WebSecurityConfig.java
+++ ss14_spring_security/exercise/blog_app/src/main/java/com/codegym/config/WebSecurityConfig.java
@@ -52,7 +52,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         // Trang /userInfo yêu cầu phải login với vai trò ROLE_USER hoặc ROLE_ADMIN.
         // Nếu chưa login, nó sẽ redirect tới trang /login.
-        http.authorizeRequests().antMatchers("/userInfo","/home","/showDetail").access("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')");
+        http.authorizeRequests().antMatchers("/userInfo","/home","/showDetail/**").access("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')");
 
         // Trang chỉ dành cho ADMIN
         http.authorizeRequests().antMatchers("/admin","/showAdd","/showUpdate","/delete","/showDetail").access("hasRole('ROLE_ADMIN')");
