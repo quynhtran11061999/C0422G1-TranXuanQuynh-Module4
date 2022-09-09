cec9b8fb4f2a6f2acfe433654eb35ef9384a9526
quynhtran11061999 <quynhtran11061999@gmail.com>
2022-09-09 17:26:47 +0700

    update

--- case_study/src/main/java/com/codegym/controller/ContractRestController.java
+++ case_study/src/main/java/com/codegym/controller/ContractRestController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ import java.util.List;
 
 
 @RestController
+@RequestMapping(value = "/contractRest")
 public class ContractRestController {
     @Autowired
     private IAttachFacilityService attachFacilityService;
